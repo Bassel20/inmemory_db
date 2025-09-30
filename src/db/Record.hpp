70899276fa2e82e2f78b6f6a1fcc2c7fbc2e25d6
bassel <basselsherif55@gmail.com>
2025-09-30 20:51:07 +0300

    first commit

---
+++ src/db/Record.hpp
@@ -0,0 +1,22 @@
+// Definition of the data record (struct/class with attributes).  
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Record {
+    private:
+        int id;
+        string name;
+
+    public:
+        Record(){};
+
+        Record(string name){
+            this -> name = name;
+        }
+
+        void Delete(int id){
+            
+        }
+
+};
